85da2d5ff92769ff13ef2a5c5d6250d800ae91de
Shael Rosen <shaelrosen@Mac.localdomain>
2026-07-17 11:38:32 -0400

    Bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ SOFTWARE.
 
 setup(
     name="python-ecobee-api",
-    version="0.5.0",
+    version="0.6.0",
     description="Python API for talking to Ecobee thermostats",
     url="https://github.com/nkgilley/python-ecobee-api",
     author="Nolan Gilley",
